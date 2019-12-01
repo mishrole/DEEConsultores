c91a315377acbbb53ca8c1d542a46b75fad4bc3e
Mitchell Rodríguez <mitrodle@gmail.com>
2019-11-30 20:00:41 -0500

    Avance 30/11/19

---
+++ resources/js/main.js
@@ -0,0 +1,177 @@
+const sFormulacion = 
+[
+    ["Ministerio de Energía y Minas - MINEM. Dirección General de Planeamiento y Presupuesto.",
+    "Servicio de consultoría para la identificación y propuesta de proyectos estándar de distribución eléctrica.",
+    "Agosto 2019"],
+    ["Unidad Ejecutora 003: Programa de Modernización del Sistema de Administración de Justicia (PMSAJ). Ministerio de Justicia y Derechos Humanos.",
+    "Servicio de Consultoría para la elaboración de costos y presupuestos de la inversión y de la operación y mantenimiento sin y con proyecto de los establecimientos de Defensa Pública (ALEGRAS - MEGA ALEGRAS).",
+    "Desde Febrero 2019 - Hasta Abril 2019"],
+    ["Compañía Minera Antamina S.A.",
+    "Servicio de Consultoría para asistir en la preparación, elaboración y sustentación del Proyecto de Inversión “Mejoramiento de los servicios para la reducción de la desnutrición y la anemia infantil en niños menores de 3 años en distritos seleccionados”.",
+    "Noviembre 2018"],
+    ["Ministerio del Ambiente - MINAM. Dirección General de Cambio Climático y Desertificación.",
+    "Consultoría para realizar la evaluación económica de seis (06) medidas de mitigación priorizadas que conforman las Contribuciones Nacionalmente Determinadas (NDC).",
+    "Desde Octubre 2018 - Hasta Diciembre 2018"],
+    ["Wildlife Conservation Society - Perú (WCS).",
+    "Servicio de Consultoría para “Analizar el Proyecto: Hidrovía Amazónica: Ríos Marañón y Amazonas, tramo Saramiriza - Iquitos - Santa Rosa; río Huallaga, tramo Yurimaguas - Confluencia con el río Marañón: río Ucayali, tramo Pucallpa - confluencia con el río Marañón, para identificar alternativas de curso de acción para mejorar el impacto positivo de la intervención y atenuar los impactos negativos”",
+    "Desde Mayo 2018 - Hasta Agosto 2018"],
+    ["Proyecto “Fomento de capacidades en desarrollo bajo en emisiones (LECB II)”. PNUD – Proyecto N° 00107022. Ministerio del Ambiente - MINAM.",
+    "Servicio de “Consultoría para la elaboración de una guía metodológica para la evaluación económica de medidas de mitigación, y aplicación sobre cuatro medidas priorizadas” en el marco del proyecto “Fomento de capacidades en desarrollo bajo en emisiones (LECB II)”. Programa de las Naciones Unidas para el Desarrollo - PNUD. Proyecto 00107022.",
+    "Desde Abril 2018 - Hasta Setiembre 2018"],
+    ["QUIMPAC S.A.",
+    "Servicios de asesoría para la preparación y presentación de Proyectos de Inversión bajo la modalidad de Obras por Impuestos.",
+    "Marzo 2018"],
+    ["Empresa de Generación Eléctrica Machupicchu S.A. EGEMSA.",
+    "Servicio de opinión especializada para definir la metodología de evaluación de la implementación de un túnel paralelo para la operación de la Central Hidroeléctrica Machupicchu, dentro del marco del sistema Invierte.Perú.",
+    "Desde Enero 2018 - Hasta Febrero 2018"],
+    ["GILAT TO HOME PERÚ S.A. Compañía Minera ANTAMINA.",
+    "Servicio de asesoría para la formulación y preparación de un Estudio de Preinversión que permita desarrollar el Proyecto de “Bibliotecas Digitales”, conjuntamente con la Compañía Minera ANTAMINA, a través del Sistema de Obras por Impuestos (Oxl) e Inverte Perú, a favor de la Municipalidad Distrital de San Marcos, Provincia de Huari, Departamento de Ancash.",
+    "Desde Enero 2018 - Hasta Setiembre 2018"],
+    ["Corporación Andina de Fomento - CAF. Ministerio del Interior de la República del Perú.",
+    "Elaboración del estudio de preinversión a nivel de Perfil del Proyecto “Implementación de un Centro de Comando, Control, Comunicaciones, Cómputo e Inteligencia - C4i para la Policía Nacional del Perú en la ciudad de Lima Metropolitana y el Callao” en el marco de lo dispuesto por el Sistema Nacional de Programación Multianual y Gestión de Inversiones del Perú (Invierte.pe).",
+    "Desde Noviembre 2017 - Hasta Agosto 2018"],
+    ["Ministerio de Comercio Exterior y Turismo - MINCETUR.",
+    "Servicio de elaboración de un Informe Técnico que sustente la verificación de viabilidad de los PIP: “Mejoramiento del servicio de seguridad turística en el corredor Julica – Puno -Isla del Lago Titicaca. Código SNIP N° 137150” y “Mejoramiento del servicio de seguridad turística en el corredor Río Amazonas, Marañón y Ucayali. Código SNIP N° 137165”.",
+    "Desde Octubre 2017 - Hasta Octubre 2017"],
+    ["Corporación Andina de Fomento - CAF. Ministerio del Interior.",
+    "Elaboración del estudio de identificación y diagnóstico para el Proyecto “Implementación de un centro de comando, control, comunicación, cómputo e inteligencia (C4i) en la ciudad de Lima y El Callao”.",
+    "Desde Setiembre 2017 - Hasta Noviembre 2017"],
+    ["Ministerio de Energía y Minas. Oficina General de Planeamiento y Presupuesto.",
+    "Consultoría para elaborar y estandarizar las fichas y contenidos mínimos para la formulación de proyectos de Electrificación Rural en el marco de Invierte.pe.",
+    "Desde Julio 2017 - Hasta Agosto 2017"],
+    ["COBRA Instalaciones y Servicios S.A. ACCIONA Infraestructuras S.A. Sucursal Perú.",
+    "Formulación del Estudio de Preinversión del Proyecto de la Iniciativa Privada Cofinanciada “Sistema de Afianzamiento Hídrico en el Valle de Ica”.",
+    "Desde Febrero 2017 - Hasta Abril 2018"],
+    ["Superintendencia Nacional de Aduanas y de Administración Tributaria - SUNAT. Gerencia de Infraestructura y Proyectos de Inversión (Unidad Formuladora).",
+    "Servicio de consultoría para la elaboración del Estudio de Pre Inversión a Nivel de Factibilidad del Proyecto de Inversión Pública: “Mejoramiento del servicio de custodia y disposición de la SUNAT para bienes y mercancías en Lima”. Código SNIP N° 338602.",
+    "Desde Setiembre 2016 - Hasta Abril 2017"],
+    ["Organismo Técnico de la Administración de los Servicios de Saneamiento – OTASS. Ministerio de Vivienda, Construcción y Saneamiento.",
+    "Servicio de consultoría para la elaboración del Estudio de Pre Inversión a Nivel de Perfil del Programa “Modernización del Sector Saneamiento”. PROG-8-2016-SNIP.",
+    "Desde Diciembre 2015 - Hasta Julio 2016"],
+    ["GMD S.A.",
+    "Consultoría para asistir técnicamente en la preparación y sustentación del IPA al MEF de la “Operación del Recaudo del Sistema de Transporte Público de Trujillo”.",
+    "Desde Julio 2015 - Hasta Mayo 2016"],
+    ["Graña & Montero S.A.A. Técnicas de Desalinización de Aguas S.A. Sucursal del Perú (TEDAGUA).",
+    "Estudios de Pre Inversión IPC - “Sistema de Tratamiento de las Aguas Residuales de la Cuenca del Lago Titicaca”.",
+    "Desde Febrero 2015 - Hasta Diciembre 2015"],
+    ["Oriental Consultants Global Co., Ltd. Formulador y Ejecutor: Programa Nacional de Conservación de Bosques – Ministerio del Ambiente. (Fondos JICA).",
+    "Consultoría para brindar el soporte técnico especializado para la elaboración de los Estudios de Factibilidad de los Proyectos de Inversión Pública en el marco del Programa: “Conservación de bosques en los departamentos de Amazonas, Lambayeque, Loreto, Piura, San Martín, Tumbes y Ucayali - PNCB – PI”. Código SNIP N° PROG-21-2011-SNIP.",
+    "Desde Octubre 2014 - Hasta Agosto 2015"],
+    ["Volcan Compañía Minera S.A.A.",
+    "Levantamiento de observaciones y reformulación del Perfil del Proyecto “Ampliación y Mejoramiento del Sistema de Agua Potable y Alcantarillado del Centro Poblado de San Agustín de Huaychao, distrito de Huayllay, Pasco” (Código SNIP N° 272749).",
+    "Desde Julio 2014 - Hasta Enero 2015"],
+    ["Rio Tinto Minera Perú",
+    "Servicio de asesoría y acompañamiento para el mejor uso del SNIP en la implementación del Proyecto Minero La Granja.",
+    "Desde Marzo 2014 - Hasta Diciembre 2015"],
+    ["BCP",
+    "Consultoría para la elaboración del Informe Técnico de Verificación de Viabilidad del Proyecto de Saneamiento del Distrito Majes - Arequipa. (Ficha SNIP Nro. 141994).",
+    "Desde Agosto 2013 - Hasta Setiembre 2013"],
+    ["Gobierno Regional de Puno",
+    "Consultoría para la Elaboración de Estudio de Factibilidad del Proyecto Fortalecimiento de la Capacidad Resolutiva con Infraestructura y Equipamiento del Gobierno Regional de Puno.",
+    "Desde Abril 2013 - Hasta Marzo 2017"],
+    ["BCP - Backus",
+    "Elaboración del Estudio de Preinversión a Nivel de perfil del Proyecto de Saneamiento en Chinchero.",
+    "Desde Enero 2013 - Hasta Junio 2013"],
+    ["XSTRATA Las Bambas",
+    "Consultoría para la Elaboración de Estudios de Pre Inversión Pública de la Nueva Urbanización de Fuerabamba (Proyectos de Cuna, Centro de Educación Inicial, Centro de Educación Primaria y Secundaria, Centro de Salud y Proyectos de Saneamiento).",
+    "Desde Abril 2012 - Hasta Enero 2014"]
+];
+
+const sElaboracion =
+[
+    ["Rio Tinto Minera Perú (RTMP)",
+    "Consultoría para la gestión de actos preparatorios para la implementación del proyecto: “Creación y construcción del puente carrozable sobre el río Checos, del centro poblado la Granja, distrito de Querocoto, provincia de Chota, departamento de Cajamarca”.",
+    "Octubre 2019"],
+    ["Proyecto Especial para la Preparación y Desarrollo de los XVIII Juegos Panamericanos del 2019. PEJP-2019. Ministerio de Transportes y Comunicaciones - MTC.",
+    "Servicio de revisión de documentos técnicos para la modificación en la fase ejecución de las IOARRs y proyecto de inversión consideradas en la Adenda Nº 02 del Contrato NEC3, opción F, denominado “Villa de Atletas”.",
+    "Junio 2019"],
+    ["Proyecto Especial para la Preparación y Desarrollo de los XVIII Juegos Panamericanos del 2019. PEJP-2019. Ministerio de Transportes y Comunicaciones - MTC.",
+    "Servicio de revisión de documentos técnicos para la modificación en la fase del Proyecto de Inversión “Mejoramiento y ampliación de los servicios del Complejo Deportivo Andrés Avelino Cáceres, Distrito de Villa María del Triunfo Provincia y Departamento de Lima”.",
+    "Desde Abril 2019 - Hasta Mayo 2019"],
+    ["Rio Tinto Minera Perú (RTMP)",
+    "Servicios de Consultoría para la elaboración del Informe Técnico para la ejecución de la obra: Rehabilitación del Pavimento de la Calle Comercio – Fundación Querocoto. Ubicado en el Distrito de Querocoto, Provincia de Chota, Departamento de Cajamarca.",
+    "Desde Noviembre 2018 - Hasta Diciembre 2018"],
+    ["Rio Tinto Minera Perú (RTMP)",
+    "Análisis y asistencia técnica de proyectos de infraestructura para revisar la información y estudios técnicos del Proyecto 258782 “Creación y construcción del puente carrozable sobre el río Checos, del centro poblado La Granja, distrito de Querocoto, provincia de Chota, departamento de Cajamarca”.",
+    "Desde Octubre 2018 - Hasta Noviembre 2018"],
+    ["Rio Tinto Minera Perú (RTMP)",
+    "Servicio de Consultoría para el “Análisis de la situación actual del Proyecto Creación y Afirmado de la Vía de Evitamiento Querocoto - Distrito de Querocoto - Cajamarca, en sus aspectos técnicos, legales y administrativos, a fin de proponer opciones para su adecuada ejecución y cierre”. Código de Proyecto: 258782.",
+    "Desde Marzo 2018 - Hasta Mayo 2018"],
+    ["Banco de Crédito del Perú S.A.A. (BCP)",
+    "Expediente Técnico del Proyecto “Creación del Servicio Educativo Especializado para alumnos de segundo grado de secundaria de Educación Básica Regular con Alto Desempeño Académico de la Región Lambayeque. Código de Proyecto N° 356151”.",
+    "Desde Octubre 2017 - Hasta Abril 2019"],
+    ["Rebaza & Alcázar Abogados SCRL (Fundación Wiese)",
+    "Elaboración y actualización del Expediente Técnico del Proyecto “Acondicionamiento turístico del Complejo Arqueológico El Brujo, en el distrito de Magdalena de Cao de la Región La Libertad. (Código SNIP N° 172122)”.",
+    "Desde Marzo 2017 - Hasta Julio 2018"],
+    ["MARCOBRE SAC",
+    "Estudios de Debida Diligencia de los Expedientes Técnicos de los Proyectos: Construcción del Puente Santa Fe y Unión Victoria y Mejoramiento del Estadio Municipal de Marcona en la localidad de Nazca, Ica.",
+    "Desde Marzo 2017 - Hasta Abril 2017"],
+    ["MINSUR S.A.",
+    "Elaboración del Perfil y Expediente Técnico para Iniciativas de Inversión Pública en los Distritos de San Miguel de Cauri y Jesús (Lauricocha, Huánuco).",
+    "Desde Setiembre 2016"],
+    ["Empresa Administradora Chungar SAC (Minera VOLCAN S.A.)",
+    "Elaboración de Expediente Técnico del Proyecto de Ampliación y Mejoramiento del Sistema de Agua Potable y Alcantarillado del Centro Poblado de San Agustín de Huaychao, distrito de Huayllay, Pasco. Código SNIP N°272749.",
+    "Desde Setiembre 2015 - Hasta Mayo 2016"],
+    ["BCP Constructora Katarindo",
+    "Elaboración de los Expedientes Técnicos de los Proyectos de “Mejoramiento, sustitución y equipamiento de la infraestructura educativa polidocente al año 2010 en la Provincia de Chincha”. Código SNIP N° 225132.",
+    "Desde Abril 2015 - Hasta Octubre 2016"],
+    ["Hudbay Peru SAC - Constancia Project",
+    "Estudio de Diseño de Carretera del Proyecto Constancia - Camino de Carga Proyecto Las Bambas.",
+    "Desde Abril 2015 - Hasta Julio 2016"],
+    ["KALLPA",
+    "Elaboración del Expediente Técnico del Proyecto “Mejoramiento de las condiciones físicas técnicas y operativas del centro cívico del distrito de Colcabamba, provincia de Tayacaja, departamento de Huancavelica” (Código SNIP N°215555).",
+    "Desde Junio 2014 - Hasta Julio 2015"],
+    ["KALLPA",
+    "Elaboración de Expediente Técnico del Proyecto “Mejoramiento de la transitabilidad peatonal y vehicular de las vías Nº 1, 2, 3, 4, 5, 7, C2, C3, C5 y C6 - Los Pinos - Distrito de Mollendo, Provincia de Islay - Arequipa”. (Ficha SNIP Nro. 233873).",
+    "Desde Febrero 2014 - Hasta Agosto 2014"],
+    ["BCP - TISUR",
+    "Elaboración del Expediente Técnico de la Obra Mirador al Pacífico “Mejoramiento de la Transitabilidad Peatonal y Vehicular en la AVIS Mirador al Pacifico del Distrito de Mollendo, Provincia de Islay - Arequipa” (Ficha SNIP Nro. 233874).",
+    "Desde Enero 2014 - Hasta Julio 2014"],
+    ["BCP - TISUR",
+    "Elaboración del Expediente Técnico de la Obra Alto Bellavista “Mejoramiento de la Transitabilidad Peatonal y Vehicular en la AVIS Alto Bellavista del Distrito de Mollendo, Provincia de Islay - Arequipa”. (Ficha SNIP Nro. 239588).",
+    "Desde Enero 2014 - Hasta Julio 2014"],
+    ["Banco de Crédito BCP",
+    "Elaboración del Expediente Técnico del Proyecto “Mejoramiento de la Institución Educativa Nro. 5142 Virgen de Guadalupe - Proyecto Especial Ciudad Pachacútec - Distrito de Ventanilla - Callao”. (Ficha SNIP Nro. 266392).",
+    "Desde Diciembre 2013 - Hasta Julio 2014"],
+    ["BCP - BACKUS",
+    "Elaboración del Expediente Técnico para la “Instalación de la Red de Alcantarillado y Tratamiento de Aguas Residuales de las Comunidades de la Microcuenca Piuray, Distrito Chinchero, Región Cusco”. (Ficha SNIP Nro. 258494).",
+    "Desde Noviembre 2013 - Hasta Junio 2014"],
+    ["Banco de Crédito BCP",
+    "Actualización del expediente técnico del proyecto de saneamiento en Villa Rica / Pasco (Ficha SNIP Nro. 14536).",
+    "Desde Setiembre, 2013 - Hasta Noviembre, 2013"]
+];
+
+let wrapper = $(".wrapper");
+
+let mostrarTabla = function(array) {
+    let tr = $("<tr></tr>");
+    let th = $("<th scope='row'>"+array[0]+"</th>");
+    let td1 = $("<td>"+array[1]+"</td>");
+    let td2 = $("<td>"+array[2]+"</td>");
+
+    tr.append(th,td1,td2);
+    return tr;
+}
+
+$(document).ready(function() {
+
+    sFormulacion.forEach(function(element) {
+        wrapper.append(mostrarTabla(element))
+    });
+
+    $("#servicesState").change(function() {
+        let selected = $(this).find("option:selected").val();
+        
+        if(selected == 1) {
+            wrapper.empty();
+            sFormulacion.forEach(function(element) {
+                wrapper.append(mostrarTabla(element))
+            });
+        }else if (selected == 2) {
+            wrapper.empty();
+            sElaboracion.forEach(function(element) {
+                wrapper.append(mostrarTabla(element))
+            });
+        }
+    });
+});
